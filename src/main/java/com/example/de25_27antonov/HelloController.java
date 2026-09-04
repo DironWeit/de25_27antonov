5ccbfd087d50f44a133c379170474b3adb03cbc8
my_pc <arseneiantonov27@gmail.com>
2026-09-04 14:30:00 +0300

    ferst commit

---
+++ src/main/java/com/example/de25_27antonov/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.de25_27antonov;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.de25_27antonov {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.de25_27antonov to javafx.fxml;
+    exports com.example.de25_27antonov;
+}
